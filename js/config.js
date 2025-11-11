100f1a2c50ebdb21262641c3c67110238fc230c4
kowshi2003 <gowshikkrajm.22bsr@kongu.edu>
2025-11-11 11:06:56 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_HnOdkMQ2j', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'bt0cukspd0244b2bndc5gkj4b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'ap-south-1_0dSAj3nXy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1m76h719rhojd0t8leonc5agr9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://vpdv0dwyeh.execute-api.us-east-1.amazonaws.com/Dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
