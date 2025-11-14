6627344f4818b1368572f7fc37c4899968f2180a
kowshi2003 <gowshikkrajm.22bsr@kongu.edu>
2025-11-14 15:31:21 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_c9KqVrVy7', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5ovn87ut1bhdq9k19pattmfl97', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_CvvZH69TP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1te00qr4caqskmcek8dd6abags', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://85g71rh3ok.execute-api.ap-south-1.amazonaws.com/wild-stage' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
